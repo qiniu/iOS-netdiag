605289df3fd3bb6ecb7d96118c612204d6d09ecc
lansefengxinzi <sunfei@sunfeideMacBook-Pro.local>
2016-09-07 16:35:55 +0800

    update http test

--- NetDiagTests/QNNHttpTest.m
+++ NetDiagTests/QNNHttpTest.m
@@ -31,7 +31,7 @@
 
 - (void)testOK {
     __block BOOL run = NO;
-    [QNNHttp start:@"https://www.baidu.com" output:[[QNNTestLogger alloc] init] complete:^(QNNHttpResult* r) {
+    [QNNHttp start:@"http://www.baidu.com" output:[[QNNTestLogger alloc] init] complete:^(QNNHttpResult* r) {
         XCTAssertNotNil(r, @"need result");
         XCTAssertNotNil(r.ip, @"need ip");
         XCTAssertNotNil(r.headers, @"need headers");
